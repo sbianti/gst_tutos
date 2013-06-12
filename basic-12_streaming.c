71c0b54eb40cb50a855d02c3bb7288651e66bff5
Sébastien Bianti <sebastienbianti@free.fr>
2013-06-13 00:36:58 +0200

    [Basic tuto 12: Streaming] Reformat code

--- basic-12_streaming.c
+++ basic-12_streaming.c
@@ -74,11 +74,10 @@ int main(int argc, char *argv[]) {
     else if (argv[1][0] == '~')
       uri = g_strdup_printf("playbin uri=\"file://%s%s\"",
 			    g_get_home_dir(), argv[1]+1);
+    else if (g_file_test(argv[1], G_FILE_TEST_IS_REGULAR))
+      uri = g_strdup_printf("playbin uri=\"file://%s\"", argv[1]);
     else
-      if (g_file_test(argv[1], G_FILE_TEST_IS_REGULAR))
-	uri = g_strdup_printf("playbin uri=\"file://%s\"", argv[1]);
-      else
-	uri = NULL;
+      uri = NULL;
   } else
     uri = NULL;
 
